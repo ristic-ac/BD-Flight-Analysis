bed00981204704387d3439a3be88ac433f4fefe9
ristic-ac <ristic.in30.2019@uns.ac.rs>
2024-01-04 19:55:28 +0100

    small improvements

--- scripts/kafka/create_topics.sh
+++ scripts/kafka/create_topics.sh
@@ -1,3 +1,4 @@
 # Kafka broker
+docker exec -it kafka1 bash
 ./usr/bin/kafka-topics --create --topic flights_germany --partitions 1 --replication-factor 1 --zookeeper zookeeper:2181
 ./usr/bin/kafka-topics --list --zookeeper zookeeper:2181
--- scripts/mongo/mongo.sh
+++ scripts/mongo/mongo.sh
@@ -1,5 +1,5 @@
-docker exec -it mongo bash
-mongo
+docker exec -it mongodb bash
+mongosh
 use flights
 db.createCollection("flights_germany")
 db.flights_germany.find().pretty()

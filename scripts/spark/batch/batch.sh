3a858411931304231fd11f22b7ad196504befa10
ristic-ac <ristic.in30.2019@uns.ac.rs>
2024-01-04 19:58:27 +0100

    additional command

--- scripts/spark/batch/batch.sh
+++ scripts/spark/batch/batch.sh
@@ -1,4 +1,5 @@
 # Batch
 # https://www.mongodb.com/docs/spark-connector/v3.0/
 docker cp /home/branislav/Downloads/mongo-spark-connector_2.12-3.0.2.jar spark-master:/spark/jars/
+docker exec -it spark-master bash
 ./spark/bin/spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.2 /spark/batch/query1.py 

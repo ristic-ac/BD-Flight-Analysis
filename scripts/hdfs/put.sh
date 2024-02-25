34a84ee1ec1da3976f0cb9edc5238511c130ecfc
Ristic Branislav <ristic.in30.2019@uns.ac.rs>
2024-02-25 20:41:31 +0100

    sync

--- scripts/hdfs/put.sh
+++ scripts/hdfs/put.sh
@@ -1,3 +1,9 @@
-docker exec -it namenode bash
-hadoop fs -mkdir /data
-hadoop fs -put /data/itineraries_sample.csv /data/
+#!/bin/bash
+
+echo "Loading data into HDFS"
+
+docker exec -it namenode bash -c "hadoop fs -mkdir /data"
+docker exec -it namenode bash -c "hadoop fs -put /data/itineraries_sample.csv /data/"
+docker exec -it namenode bash -c "hadoop fs -ls /data"
+
+echo "Data loaded into HDFS"
--- scripts/kafka/create_topics.sh
+++ scripts/kafka/create_topics.sh
@@ -1,4 +1,8 @@
-# Kafka broker
-docker exec -it kafka1 bash
-./usr/bin/kafka-topics --create --topic flights_germany --partitions 1 --replication-factor 1 --zookeeper zookeeper:2181
-./usr/bin/kafka-topics --list --zookeeper zookeeper:2181
+#!/bin/bash
+
+echo "Creating Kafka topics"
+
+docker exec -it kafka1 bash -c "/usr/bin/kafka-topics --create --topic flights_germany --partitions 1 --replication-factor 1 --zookeeper zookeeper:2181"
+docker exec -it kafka1 bash -c "/usr/bin/kafka-topics --list --zookeeper zookeeper:2181"
+
+echo "Kafka topics created"

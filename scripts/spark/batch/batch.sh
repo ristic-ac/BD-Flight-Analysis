aa5ba37b719260c9341363fec75c0741cee6a011
Ristic Branislav <ristic.in30.2019@uns.ac.rs>
2024-03-03 12:04:25 +0100

    removed dot in absolute path

--- scripts/spark/batch/batch.sh
+++ scripts/spark/batch/batch.sh
@@ -3,6 +3,6 @@
 echo "Executing Spark batch job"
 
 # https://www.mongodb.com/docs/spark-connector/v3.0/
-docker exec -it spark-master bash -c "./spark/bin/spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.2 /spark/batch/query1.py "
+docker exec -it spark-master bash -c "/spark/bin/spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.2 /spark/batch/query1.py "
 
 echo "Spark batch job executed"

a32d640f29aad404b34ab9df29e129f64ac9776b
Ristic Branislav <ristic.in30.2019@uns.ac.rs>
2024-03-03 12:03:15 +0100

    batch conflict fixed

--- scripts/spark/batch/batch.sh
+++ scripts/spark/batch/batch.sh
@@ -3,12 +3,6 @@
 echo "Executing Spark batch job"
 
 # https://www.mongodb.com/docs/spark-connector/v3.0/
-<<<<<<< HEAD
-docker cp /home/branislav/Downloads/mongo-spark-connector_2.12-3.0.2.jar spark-master:/spark/jars/
-docker exec -it spark-master bash
-./spark/bin/spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.2 /spark/batch/query1.py 
-=======
 docker exec -it spark-master bash -c "./spark/bin/spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.2 /spark/batch/query1.py "
 
 echo "Spark batch job executed"
->>>>>>> c631e14 (sync)
